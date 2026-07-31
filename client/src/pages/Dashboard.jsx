66bdddfb3800f77d945cebf5decddc674af5c71a
alloy01 <ajeettsingh51@gmail.com>
2026-07-31 13:03:40 +0530

    basic /dashboard page (only structure right now no functionality)

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,18 @@
 const Dashboard = () => {
     return(
-        <div>
-            <p>This is dashboard.</p>
+        <div className="bg-black min-h-screen">
+            <div className="flex justify-between py-8 px-8 items-center text-slate-100 font-mono">
+                <div>
+                    <p className="">username</p>
+                </div>
+                <p                         className=" text-2xl underline underline-offset-12">StockPilot📦- Dashboard
+                </p>
+                <div>
+                    <button className=" border-2 border-slate-100 px-2 py-1">
+                        logout
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
